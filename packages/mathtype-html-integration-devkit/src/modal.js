2ae97b9fec0b60704ad83ff81ac12a333fb20ba9
yunmingchun <yunmingchun@gmail.com>
2022-06-27 10:25:40 +0800

    wip: 注释掉 telemetry.wiris.net 慢接口

--- packages/mathtype-html-integration-devkit/src/modal.js
+++ packages/mathtype-html-integration-devkit/src/modal.js
@@ -474,22 +474,22 @@ export default class ModalDialog {
    */
   open() {
     // TODO Change the call to telemetry to use events.
-    try {
-      TelemetryService.send([{
-        timestamp: new Date().toJSON(),
-        topic: '0',
-        level: 'info',
-        message: 'HELO telemetry.wiris.net',
-      }])
-        .then((response) => { // eslint-disable-line no-unused-vars
-          // TODO: manage retries for codes
-          // DEBUG
-          // console.log('modal.open TelemetryService.send - response:', response);
-        });
-    } catch (error) {
-      // DEBUG
-      // console.log('modal.open TelemetryService.send - error:', error);
-    }
+    // try {
+    //   TelemetryService.send([{
+    //     timestamp: new Date().toJSON(),
+    //     topic: '0',
+    //     level: 'info',
+    //     message: 'HELO telemetry.wiris.net',
+    //   }])
+    //     .then((response) => { // eslint-disable-line no-unused-vars
+    //       // TODO: manage retries for codes
+    //       // DEBUG
+    //       // console.log('modal.open TelemetryService.send - response:', response);
+    //     });
+    // } catch (error) {
+    //   // DEBUG
+    //   // console.log('modal.open TelemetryService.send - error:', error);
+    // }
 
     // Removing close class.
     this.removeClass('wrs_closed');
